0e7201b10cc9058e092cbce25f4b17edea73f66d
attilakoksal <attila.b.koksal@gmail.com>
2023-04-10 01:34:08 -0700

    Add initial files

---
+++ src/shape_main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../header/rectangle.hpp"
+#include "../header/triangle.hpp"
+
+using namespace std;
+
+int main()
+{
+    
+
+}
---
+++ src/triangle.cpp
@@ -0,0 +1,12 @@
+#include "../header/triangle.hpp"
+
+void Triangle::set_base(int b){
+    this->base = b;
+}
+void Triangle::set_height(int h){
+    this->height = h;
+}
+
+int Triangle::area(){
+    return (this->base * this->height)/2;
+}
